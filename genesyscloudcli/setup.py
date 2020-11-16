6ce5f20899b5eb810a31525e31230812dca5b2ee
Kevin <kevin.glinski@mypureinsights.com>
2020-11-16 15:26:59 -0500

    adding dep to requests

--- genesyscloudcli/setup.py
+++ genesyscloudcli/setup.py
@@ -7,7 +7,8 @@ setup(
     install_requires=[
         'Click',
         'pyyaml',
-        'websockets'
+        'websockets',
+        'requests'
     ],
     entry_points='''
         [console_scripts]
